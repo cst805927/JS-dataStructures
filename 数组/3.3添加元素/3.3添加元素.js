2b72e34fb02e74bb93da4adb2b4fd089c5ea15c6
cst805927 <59718729+cst805927@users.noreply.github.com>
2022-01-20 17:22:01 +0800

    Delete 数组 directory

--- 数组/3.3添加元素/3.3添加元素.js
+++
@@ -1,8 +0,0 @@
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-//1 在数组末尾插入元素
-numbers.push(11)
-numbers.push(12, 13)
-
-//2 在数组开头插入元素
-numbers.unshift(-2)
-numbers.unshift(-4, -3)
--- 数组/3.7JS的数组方法参考/3.7.1数组合并.js
+++
@@ -1,6 +0,0 @@
-//3.7.1 数组合并
-const zero = 0
-const positiveNumbers = [1, 2, 3]
-const negativeNumbers = [-3, -2, -1]
-
-let numbers = negativeNumbers.concat(zero, positiveNumbers) //[-3, -2, -1, 0, 1, 2, 3]
