a52183ba900797db0480cb841c7366c2271a76da
cst805927 <cst698@163.com>
2022-01-20 17:07:43 +0800

    第三章

---
+++ 数组/3.6二维和多维数组/3.6.0二维和多维数组.js
@@ -0,0 +1,20 @@
+//JS只支持一维数组，不支持矩阵
+//用数组套数组，实现矩阵或任一多维数组
+
+//day1
+const averageTemp = []
+averageTemp[0] = []
+averageTemp[0][0] = 72
+averageTemp[0][1] = 75
+averageTemp[0][2] = 79
+averageTemp[0][3] = 79
+averageTemp[0][4] = 81
+averageTemp[0][5] = 81
+//day2
+averageTemp[0] = []
+averageTemp[0][0] = 72
+averageTemp[0][1] = 75
+averageTemp[0][2] = 79
+averageTemp[0][3] = 79
+averageTemp[0][4] = 81
+averageTemp[0][5] = 81
